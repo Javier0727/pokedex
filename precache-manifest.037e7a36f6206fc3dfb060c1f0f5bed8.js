9c1143f6d5a2d2c47b0f631af88af7050658a216
javier <javier.castillo@studentpartner.com>
2020-01-25 16:34:34 -0600

    Updates

---
+++ precache-manifest.037e7a36f6206fc3dfb060c1f0f5bed8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2309706477b6d2ff290c7f41baf7318b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "775b03b96c24406bb2dd",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8406e7f25fa9f91136d7",
+    "url": "/static/css/main.0dcdff06.chunk.css"
+  },
+  {
+    "revision": "775b03b96c24406bb2dd",
+    "url": "/static/js/2.6571dc6e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.6571dc6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8406e7f25fa9f91136d7",
+    "url": "/static/js/main.68533420.chunk.js"
+  },
+  {
+    "revision": "a3ae24c73f7b90477b95",
+    "url": "/static/js/runtime-main.801dd740.js"
+  },
+  {
+    "revision": "09a6ae937a6d9ef5cd10d132b59d6f5d",
+    "url": "/static/media/pokeball_PNG19.09a6ae93.png"
+  }
+]);
